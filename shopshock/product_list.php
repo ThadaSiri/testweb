27f9fe5ffe7850660b0cebaf2cc0ff222fa8770f
SaKuRaSouE <6306021412016@fitm.kmutnb.ac.th>
2021-04-02 16:05:11 +0700

    update

--- shopshock/product_list.php
+++ shopshock/product_list.php
@@ -11,7 +11,13 @@
     <title>Document</title>
 </head>
 <body onload= "load_doc()">
-<center><div id="out"></div><br><div id="out2"></div><center>
+<center>
+<div id="out"></div>
+<br>
+<div id="out2"></div>
+<br>
+<div id="out3"></div><div id="out4"></div>
+<center>
     <script>
     let cus_id = 1;  
     let arr;
@@ -57,6 +63,8 @@
     }
 
     function open_po(idx){
+        let out = document.getElementById("out3");
+        let out2 = document.getElementById("out4");
         $p_qty = document.getElementById("n_"+idx);
         xhttp = new XMLHttpRequest();
         $p_id    = arr[idx][0]; //id
@@ -64,11 +72,48 @@
         xhttp.onreadystatechange = function(){
             if(this.readyState==4 && this.status==200){
                 alert(this.responseText);
+                arr = JSON.parse(this.responseText);
+
+                bill = arr[0];
+                bill_detail = arr[1];
+                text = "<table border='1'>";
+                text += "<tr><td>Bill ID</td><td>CUS_ID</td><td>EMP_ID</td><td>Bill_Date</td><td>Bill STATUS</td><td>Comment</td><td></td></tr>";
+                text += "<tr>"
+                for(i=0;i<bill.length;i++){
+                    text+= "<th>"+bill[i]+"</th>"; 
+                }
+                text += "<td><a href=''>Paid Bill</a></td></tr></table>";
+                out.innerHTML = text;
+
+                bill_detail = arr[1];
+                text2 = "<table border='1'>";
+                text2 += "<tr><td>ITEM No.</td><td>PRODUCT_CODE</td><td>Product_Name</td><td>Quantity</td><td>PRICE</td><td>Total_Price</td><td></td></tr>";
+                for(i=0;i<bill_detail.length;i++){
+                    text2 += "<tr><td>"+(i+1)+"</td>";
+                    for(j=1;j<bill_detail[i].length;j++){
+                        text2 += "<td>"+ bill_detail[i][j]+"</td>";
+                    }
+                    text2 = text2+"<td><button onclick='del_product("+bill[0]+","+bill_detail[i][0]+")'>Remove from Cart</button></td></tr>";
+                }
+                text2 += "</table>";
+                out2.innerHTML = text2;
             }
         }
         xhttp.open("POST","product_rest.php",true);
-        xhttp.setRequestHeader("content-type","application/x-www-form-urlencoded");
+        xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
         xhttp.send("p_id="+$p_id+"&p_price="+$p_price+"&p_qty="+$p_qty.value);
+    }
+
+    function del_product(bill_id, p_code){
+        alert(p_code);
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function(){
+            if(this.readyState==4 && this.status==200){
+                alert(this.responseText);
+            }
+        }
+        xhttp.open("DEL","product_rest.php?bill_id="+bill_id+"&id="+p_code,true);
+        xhttp.send();
     }
     </script>
 </body>
--- shopshock/product_rest.php
+++ shopshock/product_rest.php
@@ -7,7 +7,8 @@
         echo json_encode(product_list(),JSON_UNESCAPED_UNICODE);
     }else if($_SERVER["REQUEST_METHOD"]=='POST'){
         echo json_encode(create_bill($_POST));
-
+    }else if($_SERVER["REQUEST_METHOD"]=='DEL'){
+        echo json_encode([del_bill_detial($_REQUEST)]);
     }
     function product_list(){
         $db = new database();
@@ -22,39 +23,63 @@
         $db->close();
         return $result;
     }
-    function create_bill($post){
+    function create_bill(){
         $db = new database();
         $db->connect();
-        //1 check first bill?
-        //1.1 yes bill_id = 1
-        //        add bill_detail -> bill_id, product_id, qty, price
-        //1.2 no check last bill_status of user?
-        //1.3    check p_id is not exist
-        //       yes add productId qty price
-        //       no update product qty
-        //1.4 new bill but not first bill
-        //       bill_id = last_id
-        $sql = "SELECT Bill_id, Bill_Status FROM bill ORDER BY Bill_id desc LIMIT 1";
+        $sql = "SELECT Bill_id, Bill_Status FROM bill order by Bill_id desc limit 1";
+        $bill_id = 1;
         $bill_result = $db->query($sql);
         $p_id    = $_POST['p_id'];
         $p_price = $_POST['p_price'];
         $p_qty   = $_POST['p_qty'];
         if(sizeof($bill_result)==0){
-            $sql = "INSERT INTO bill(Bill_id, Cus_ID, Bill_Status) VALUES (1,{$_SESSION['cus_id']},0)";
+            $sql = "INSERT INTO bill(Bill_id, Cus_ID, Bill_Status) VALUES ({$bill_id},{$_SESSION['cus_id']},0)";
             $result = $db->exec($sql);
-            $sql = "INSERT INTO bill_detail(Bill_id, Product_ID, Quantity, Unit_Price) VALUES (1,{$p_id},{$p_qty},{$p_price})";
+            $sql = "INSERT INTO bill_detail(Bill_id, Product_ID, Quantity, Unit_Price) 
+                    VALUES (1,{$p_id},{$p_qty},{$p_price})";
             $result = $db->exec($sql);
         }else{
-            $sql = "SELECT Bill_id, Bill_Status FROM bill WHERE Cus_ID={$_SESSION['cus_id']} ORDER BY Bill_id desc LIMIT 1";
+            $sql = "SELECT Bill_id, Bill_Status FROM bill WHERE Cus_ID={$_SESSION['cus_id']} order by Bill_id desc limit 1";
             $user_bill_result = $db->query($sql);
-            if(sizeof($user_bill_result)==1 && $user_bill_result[0][1] == 0){
+            if(sizeof($user_bill_result)==1&&$user_bill_result[0][1]==0){
                 $bill_id = $user_bill_result[0][0];
-                $sql = "INSERT INTO bill_detail(Bill_id, Product_ID, Quantity, Unit_Price) VALUES ({$bill_id},{$p_id},{$p_qty},{$p_price})";
+                $sql = "INSERT INTO bill_detail(Bill_id, Product_ID, Quantity, Unit_Price) 
+                VALUES ({$bill_id},{$p_id},{$p_qty},{$p_price})";
                 $result = $db->exec($sql);
-
-                
+                if($result==0){
+                    //update product
+                    $sql = "UPDATE bill_detail SET Quantity={$p_qty},Unit_Price={} WHERE Bill_id = {$bill_id} AND Product_ID = {$p_id}";
+                    $result = $db->exec($sql);
+                }
+            }else{
+                //create new bill
+                $bill_id = $bill_result[0][0]+1;
+                $sql = "INSERT INTO bill(Bill_id, Cus_ID, Bill_Status) VALUES ({$bill_id},{$_SESSION['cus_id']},0)";
+                $result = $db->exec($sql);
+                $sql = "INSERT INTO bill_detail(Bill_id, Product_ID, Quantity, Unit_Price) 
+                        VALUES ({$bill_id},{$p_id},{$p_qty},{$p_price})";
+                $result = $db->exec($sql);
             }
+            //show_shoppingcart
+            $sql = "SELECT * FROM bill where Bill_id={$bill_id}";
+            $user_bill_result = $db->query($sql,MYSQLI_NUM);
+            $sql = "SELECT product.Product_id, product.Product_code, product.Product_Name, Quantity,Unit_Price, 
+                                (Quantity*Unit_Price) AS Total 
+                    FROM bill_detail, product 
+                    WHERE bill_detail.Product_ID = product.Product_id AND Bill_id={$bill_id}";
+            $user_bill_result2 = $db->query($sql,MYSQLI_NUM);
         }
+        $db->close();
+        $myresult = [$user_bill_result[0], $user_bill_result2];
+        return $myresult;
+    }
+
+    function del_bill_detial(){
+        $db = new database();
+        $db->connect();
+        $sql = "DELETE FROM bill_detail WHERE Bill_id={$_GET['bill_id']} AND Product_ID={$_GET['id']}";
+        //echo json_encode($sql);
+        $result = $db->exec($sql);
         $db->close();
         return $result;
     }
